c2d8f63ee61219cd0b373185797f08a280a6ee02
Carolin Alfers <alfersca82660@th-nuernberg.de>
2022-05-21 09:43:03 +0200

    Console App. completion

--- AlgoDat_Praktikum/AlgoDat_Praktikum/Program.cs
+++ AlgoDat_Praktikum/AlgoDat_Praktikum/Program.cs
@@ -53,132 +53,201 @@ namespace AlgoDat_Praktikum
             IDictionary structure = null;
             string res;
 
-            do
+            while (true)
+            //keeps trying until no error has occured
             {
-                Console.Write("Your Choice: ");
-                res = Console.ReadLine();
-                switch (res)
+                try
                 {
-                    case "1":
-                        bool[] ans = checkQuestions();
-                        //if (ans[0])
-                        //{
-                        //    if (ans[1])
-                        //        structure = new MultiSetUnsortedArray();
-                        //          Console.WriteLine("You chose an unsorted array multi set.");
-                        //    else
-                        //        structure = new MultiSetSortedArray();
-                        //          Console.WriteLine("You chose a sorted array multi set.");
-                        //}
-                        //else
-                        //{
-                        //    if (ans[1])
-                        //        structure = new SetUnsortedArray();
-                        //Console.WriteLine("You chose an unsorted array set.");
-                        //    else
+                    do
+                    {
+                        Console.Write("Your Choice: ");
+                        res = Console.ReadLine();
+                        switch (res)
+                        {
+                            case "1":
+                                bool[] ans = checkQuestions();
+                                //if (ans[0])
+                                //{
+                                //    if (ans[1])
+                                //        structure = new MultiSetUnsortedArray();
+                                //          Console.WriteLine("You chose an unsorted array multi set.");
+                                //    else
+                                //        structure = new MultiSetSortedArray();
+                                //          Console.WriteLine("You chose a sorted array multi set.");
+                                //}
+                                //else
+                                //{
+                                //    if (ans[1])
+                                //        structure = new SetUnsortedArray();
+                                //Console.WriteLine("You chose an unsorted array set.");
+                                //    else
 
-                        //        structure = new SetSortedArray();
-                        //Console.WriteLine("You chose an sorted array set.");
-                        //}
-                        break;
-                    case "2":
-                        ans = checkQuestions();
-                        if (ans[0])
-                        {
-                            if (ans[1])
-                            {
-                                structure = new MultiSetUnsortedLinkedList();
-                                Console.WriteLine("You chose an unsorted linked list multi set.");
-                            }
-                            else
-                            {
-                                structure = new MultiSetSortedLinkedList();
-                                Console.WriteLine("You chose a sorted linked list mulit set.");
-                            }
+                                //        structure = new SetSortedArray();
+                                //Console.WriteLine("You chose an sorted array set.");
+                                //}
+                                break;
+                            case "2":
+                                ans = checkQuestions();
+                                if (ans[0])
+                                {
+                                    if (ans[1])
+                                    {
+                                        structure = new MultiSetUnsortedLinkedList();
+                                        Console.WriteLine("You chose an unsorted linked list multi set.");
+                                    }
+                                    else
+                                    {
+                                        structure = new MultiSetSortedLinkedList();
+                                        Console.WriteLine("You chose a sorted linked list mulit set.");
+                                    }
+                                }
+                                else
+                                {
+                                    if (ans[1])
+                                    {
+                                        structure = new SetUnsortedLinkedList();
+                                        Console.WriteLine("You chose an unsorted linked list set.");
+                                    }
+                                    else
+                                    {
+                                        structure = new MultiSetSortedLinkedList();
+                                        Console.WriteLine("You chose a sorted linked list set.");
+                                    }
+                                }
+                                break;
+                            case "3":
+                                Console.Write("For QuadProb, please press Q. For SepChain, please press S: ");
+                                if (Console.ReadLine().ToLower() == "q")
+                                {
+                                    Console.Write("\nFor Div, please press D. For Mult, please press M: ");
+                                    if (Console.ReadLine().ToLower() == "d")
+                                    {
+                                        structure = new HashTabQuadProb();
+                                    }
+                                    else
+                                    {
+                                        //I don't know which hash type this is
+                                    }
+                                }
+                                else
+                                {
+                                    Console.Write("\nFor Div, please press D. For Mult, please press M: ");
+                                    if (Console.ReadLine().ToLower() == "d")
+                                    {
+                                        structure = new HashTabSepChain<SetUnsortedLinkedList>();
+                                    }
+                                    else
+                                    {
+
+                                        //I don't know which hash type this is
+                                    }
+                                }
+                                Console.WriteLine();
+                                Console.WriteLine("You chose a Hash table.");
+                                break;
+                            case "4":
+                                structure = new BinSearchTree();
+                                Console.WriteLine("You chose a Binary Search Tree.");
+                                break;
+                            case "5":
+                                structure = new AVLTree();
+                                Console.WriteLine("You chose an AVL-Tree.");
+                                break;
+                            case "6":
+                                structure = new Treap();
+                                Console.WriteLine("You chose a Treap.");
+                                break;
+                            default:
+                                Console.WriteLine("Sorry, we didn't recognize that input. Please try again!");
+                                res = "y";
+                                break;
                         }
-                        else
+                        if (res != "y")
                         {
-                            if (ans[1])
-                            {
-                                structure = new SetUnsortedLinkedList();
-                                Console.WriteLine("You chose an unsorted linked list set.");
-                            }
-                            else
-                            {
-                                structure = new MultiSetSortedLinkedList();
-                                Console.WriteLine("You chose a sorted linked list set.");
-                            }
+                            Console.Write("Do you want to change your choice? (y/n) ");
+                            res = Console.ReadLine();
                         }
-                        break;
-                    case "3":
-                        // Lukas Hash Implementierung
-                        Console.WriteLine("You chose a Hash table.");
-                        break;
-                    case "4":
-                        structure = new BinSearchTree();
-                        Console.WriteLine("You chose a Binary Search Tree.");
-                        break;
-                    case "5":
-                        structure = new AVLTree();
-                        Console.WriteLine("You chose an AVL-Tree.");
-                        break;
-                    case "6":
-                        structure = new Treap();
-                        Console.WriteLine("You chose a Treap.");
-                        break;
-                    default:
-                        Console.WriteLine("Sorry, we didn't recognize that input. Please try again!");
-                        res = "y";
-                        break;
+                    } while (res != "n");
+                    break;
                 }
-                if (res != "y")
+                catch
                 {
-                    Console.Write("Do you want to change your choice? (y/n) ");
-                    res = Console.ReadLine();
+                    Console.WriteLine("An unforeseen error has occured. Please try again!");
                 }
-            } while (res != "n");
+            }
 
             Console.WriteLine("Wonderful! Now you can work with your chosen data structure.");
-            Console.WriteLine("Press Enter.");
+            Console.WriteLine("Press enter to continue.");
             while (Console.ReadKey(true).Key != ConsoleKey.Enter) ; //waits for enter to be pressed without displaying input
-            
+            Console.Clear();
+
             bool proceed = true;
             int data;
-            do
+            string command;
+            while (true)
+            //keeps trying until no error has occured
             {
-                Console.Clear();
-                Console.WriteLine("Your current structure: ");
-                structure.print();
-                Console.WriteLine("\nYou can insert or delete data or you can search for data you already stored.");
-                Console.WriteLine("To do this, please first enter which command you wish to use. Then enter your data in the next line.");
-                Console.WriteLine("To exit the programm , please enter exit.");
-                Console.Write("\nPlease enter a command: ");
-                string command = Console.ReadLine().ToLower();
-                switch (command)
+                try
                 {
-                    case "insert":
-                        Console.Write("Please enter your data: ");
-                        data = Convert.ToInt32(Console.ReadLine());
-                        structure.insert(data);
-                        break;
-                    case "delete":
-                        Console.Write("Please enter your data: ");
-                        data = Convert.ToInt32(Console.ReadLine());
-                        structure.delete(data);
-                        break;
-                    case "search":
-                        Console.Write("Please enter your data: ");
-                        data = Convert.ToInt32(Console.ReadLine());
-                        structure.search(data);
-                        break;
-                    case "exit":
-                        proceed = false;
-                        break;
-                    default:
-                        Console.WriteLine("Sorry, we didn't recognize that command. Please try again!");
-                        break;
+
+                    do
+                    {
+                        Console.WriteLine("\nYou can insert or delete data or you can search for data you already stored.");
+                        Console.WriteLine("To do this, please first enter which command you wish to use. Then enter your data in the next line.");
+                        Console.WriteLine("To exit the programm, please enter exit.");
+                        Console.Write("\nPlease enter a command: ");
+                        command = Console.ReadLine().ToLower();
+                        Console.Clear();
+
+                        Console.WriteLine("Your previous structure:\n");
+                        structure.print();
+                        Console.WriteLine($"\n\nPlease enter a command: {command}");
+
+                        switch (command)
+                        {
+                            case "insert":
+                                Console.Write("Please enter your data: ");
+                                data = Convert.ToInt32(Console.ReadLine());
+                                if (!structure.insert(data))
+                                {
+                                    Console.WriteLine("This key could not be stored.");
+                                }
+                                break;
+                            case "delete":
+                                Console.Write("Please enter your data: ");
+                                data = Convert.ToInt32(Console.ReadLine());
+                                if (!structure.delete(data))
+                                {
+                                    Console.WriteLine("The key was not found and therefore could not be deleted ´.");
+                                }
+                                break;
+                            case "search":
+                                Console.Write("Please enter your data: ");
+                                data = Convert.ToInt32(Console.ReadLine());
+                                if (!structure.search(data))
+                                {
+                                    Console.WriteLine("This key was not yet part of your data structure. It was inserted into it now.");
+                                }
+                                break;
+                            case "exit":
+                                proceed = false;
+                                break;
+                            default:
+                                Console.Write("Sorry, we didn't recognize that command. Please try again!");
+                                break;
+                        }
+
+                        Console.WriteLine("\nYour current structure:\n");
+                        structure.print();
+                        Console.WriteLine();
+                    } while (proceed);
+                    break;
                 }
-            } while (proceed);
+                catch
+                {
+                    Console.WriteLine("That didn't work. It seems an unforeseen error has occured. Please try again."); ;
+                }
+            }
 
             Console.WriteLine("Thank you for using our programm!");
         }
